ce4a183bc775b17bdfee7179458dfa782d67e09b
thlinhdotcom <47789314+thlinhdotcom@users.noreply.github.com>
2023-03-30 10:15:20 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apt-get -y install squid && systemctl enable squid && sed -i 's/http_access deny all/http_access allow all/' /etc/squid/squid.conf && sed -i 's/http_port 3128/http_port 8181/' /etc/squid/squid.conf && ufw allow && service squid restart && sleep 1;
+apt-get -y install squid && systemctl enable squid && sed -i 's/http_access deny all/http_access allow all/' /etc/squid/squid.conf && sed -i 's/http_port 3128/http_port 8181/' /etc/squid/squid.conf && service squid restart && sleep 1;
